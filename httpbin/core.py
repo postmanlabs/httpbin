738d82f91e4c7215ba6f596d571e7524f3e57200
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 18:08:32 -0400

    basic core

--- httpbin/core.py
+++ httpbin/core.py
@@ -7,14 +7,49 @@ httpbin.core
 This module provides the core HttpBin experience.
 """
 
+import json
 
-from flask import Flask
+
+from flask import Flask, request
 app = Flask(__name__)
+
+
+
+def to_json(*args, **kwargs):
+    data = dict(*args, **kwargs)
+    return json.dumps(data)
+
+
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return to_json(hello='world')
+
+
+@app.route('/headers')
+def view_headers():
+
+    return to_json(headers='headers')
+
+
+@app.route('/user-agent')
+def view_user_agent():
+    return 'user agent'
+
+
+@app.route('/get')
+def view_get():
+    return 'get'
+
+
+
+# /headers
+# /get
+# /post
+# /put
+# /delete
+
 
 
 if __name__ == '__main__':
