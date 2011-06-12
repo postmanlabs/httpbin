22195385c2003404d669d107550feb5bd10b6a38
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 18:08:49 -0400

    basic manage.py

---
+++ manage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from flaskext.script import Manager, Command
+
+app = Flask(__name__)
+# configure your app
+
+manager = Manager(app)
+
+
+@manager.command
+def hello():
+    """Hello World!"""
+    print r'\o/'
+
+
+if __name__ == "__main__":
+    manager.run()
