a694d726a1d2b910b0c3b9bda8e1fb5d8a1daad2
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 13:29:23 -0400

    ep.io fabfile

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from fabric.api import *
+
+
+CMD_TEMPLATE = '{0}'
+
+
+def _run(cmd):
+    local(CMD_TEMPLATE.format(cmd))
+
+
+def prod():
+    """Runs all command on the production instance."""
+    global CMD_TEMPLATE
+
+    CMD_TEMPLATE = 'epio run_command {0}'
+
+
+def deploy():
+    """Deploys the application"""
+
+    prod()
+    local('epio upload')
+    migrate()
+
