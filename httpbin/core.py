0626b048fabe0d5a135498f16428305cf9b6dd0c
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 14:53:40 -0400

    epio

--- httpbin/core.py
+++ httpbin/core.py
@@ -12,9 +12,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
