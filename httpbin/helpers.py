319f08033d5d1dc7afe71a65d9a2cf0dcc4d55ad
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 22:44:48 -0400

    TEAPOT POWER!

--- httpbin/helpers.py
+++ httpbin/helpers.py
@@ -8,10 +8,22 @@ This module provides helper functions for httpbin.
 """
 
 
-from flask import request
+from flask import request, make_response
 
 from .structures import CaseInsensitiveDict
 
+
+ASCII_ART = r"""
+    -=[ teapot ]=-
+
+       _...._
+     .'  _ _ `.
+    | ."` ^ `". _,
+    \_;`"---"`|//
+      |       ;/
+      \_     _/
+        `\"\"\"`
+"""
 
 
 def get_files():
@@ -25,8 +37,25 @@ def get_files():
     return files
 
 
-
 def get_headers():
     """Returns headers dict from request context."""
 
     return CaseInsensitiveDict(request.headers.items())
+
+
+def status_code(code):
+    """Returns response object of given status code."""
+
+    code_map = {
+        418: dict(data=ASCII_ART),
+    }
+
+    r = make_response()
+    r.status_code = code
+
+    if code in code_map:
+        if 'data' in code_map[code]:
+            r.data = code_map[code]['data']
+
+    print code_map.get(code)
+    return r
