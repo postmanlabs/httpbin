d1b55ddf26efa3bd52ed00cdaba136750697f3ab
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 22:50:47 -0400

    no more logging, for now

--- httpbin/core.py
+++ httpbin/core.py
@@ -12,46 +12,16 @@ import json
 import gzip
 import os
 from cStringIO import StringIO
-from time import time as now
 from decimal import Decimal
+from time import time as now
 
-import redi
 from decorator import decorator
 from flask import Flask, Response, request, render_template, redirect, g
 
-from .db import redis_connect
 from .helpers import get_files, get_headers, status_code
 
 
 app = Flask(__name__)
-
-
-# ------
-# Config
-# ------
-
-def logging_is_enabled():
-    """Returns bool to determine if logging is enabled or not."""
-
-    return os.environ.get('HTTPBIN_LOG_REQUESTS', False)
-
-
-
-# --------------
-# Pre-Processing
-# --------------
-
-if logging_is_enabled:
-
-    @app.before_request
-    def db_connect():
-        """Connects Redis to g object."""
-
-        # redis connect
-        if not getattr(g, 'r', None):
-            g.r = redis_connect()
-            redi.config.init(r=g.r)
-
 
 
 # --------------
@@ -108,26 +78,6 @@ def gzip_response(f, *args, **kwargs):
 
 
 
-
-
-def log_request(key):
-    """Logging Decorator."""
-
-    def log_request(f, *args, **kwargs):
-
-        response = f(*args, **kwargs)
-
-        if logging_is_enabled():
-            # store request in redis list
-            redi.s.httplib._(key, 'list').append(response)
-
-        return response
-
-    return decorator(log_request)
-
-
-
-
 # ------
 # Routes
 # ------
@@ -141,7 +91,6 @@ def view_landing_page():
 
 @app.route('/ip')
 @json_resource
-@log_request(key='httpbin:ip')
 def view_origin():
     """Returns Origin IP."""
 
@@ -150,7 +99,6 @@ def view_origin():
 
 @app.route('/headers')
 @json_resource
-@log_request(key='httpbin:headers')
 def view_headers():
     """Returns HTTP HEADERS."""
 
@@ -161,7 +109,6 @@ def view_headers():
 
 @app.route('/user-agent')
 @json_resource
-@log_request(key='httpbin:user-agent')
 def view_user_agent():
     """Returns User-Agent."""
 
@@ -172,7 +119,6 @@ def view_user_agent():
 
 @app.route('/get', methods=('GET',))
 @json_resource
-@log_request(key='httpbin:get')
 def view_get():
     """Returns GET Data."""
 
@@ -187,7 +133,6 @@ def view_get():
 
 @app.route('/post', methods=('POST',))
 @json_resource
-@log_request(key='httpbin:post')
 def view_post():
     """Returns POST Data."""
 
@@ -204,7 +149,6 @@ def view_post():
 
 @app.route('/put', methods=('PUT',))
 @json_resource
-@log_request(key='httpbin:put')
 def view_post():
     """Returns PUT Data."""
 
@@ -221,7 +165,6 @@ def view_post():
 
 @app.route('/delete', methods=('DELETE',))
 @json_resource
-@log_request(key='httpbin:delete')
 def view_post():
     """Returns DETLETE Data."""
 
@@ -239,7 +182,6 @@ def view_post():
 @app.route('/gzip')
 @gzip_response
 @json_resource
-@log_request(key='httpbin:gzip')
 def view_gzip_encoded_content():
     """Returns GZip-Encoded Data."""
 
