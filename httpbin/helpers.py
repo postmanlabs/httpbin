acc558ed2433de61132887f24773d3c3653cb13a
Kenneth Reitz <me@kennethreitz.com>
2011-06-12 20:39:25 -0400

    helpers

---
+++ httpbin/helpers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+httpbin.helpers
+~~~~~~~~~~~~~~~
+
+This module provides helper functions for httpbin.
+"""
+
+
+
+def get_files():
+    """Returns files dict from request context."""
+
+    files = dict()
+
+    for k, v in request.files.items():
+        files[k] = v.read()
+
+    return files
+
+
+
+def get_headers():
+    """Returns headers dict from request context."""
+
+    return CaseInsensitiveDict(request.headers.items())
