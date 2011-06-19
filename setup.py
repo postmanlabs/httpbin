2131d8e2676a03a1077a5ef90747accb299f51fa
Kenneth Reitz <me@kennethreitz.com>
2011-06-19 12:53:34 -0400

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+setup(
+    name='httpbin',
+    version='110619',
+    install_requires=open('reqs.txt').readlines(),
+    description='HTTP Request and Response Service.',
+    long_description=open('README.md').read(),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://httpbin.org',
+    packages=['httpbin'],
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3.0',
+        # 'Programming Language :: Python :: 3.1',
+        # 'Programming Language :: Python :: 3.2',
+    ),
+)
