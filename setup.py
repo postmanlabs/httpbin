7f489529e13e3135fbe098f59eb869faf484a510
Kenneth Reitz <me@kennethreitz.com>
2011-10-15 11:51:37 -0400

    v0.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='httpbin',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=open('requirements.txt').readlines(),
     description='HTTP Request and Response Service.',
     long_description=open('README.md').read(),
