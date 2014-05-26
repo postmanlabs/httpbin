431acaabf7a3e77b416a57998bfadcb2d3864555
Kevin McCarthy <me@kevinmccarthy.org>
2014-05-25 19:39:00 -1000

    Add a setup.py
    This allows tox to run, and allows me to install httpbin as a depenency
    (see #136)

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+setup(
+    name="httpbin",
+    version="0.1.0",
+    description="HTTP Request and Response Service",
+
+    # The project URL.
+    url='https://github.com/kennethreitz/httpbin',
+
+    # Author details
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+         'Development Status :: 5 - Production/Stable',
+         'Intended Audience :: Developers',
+         'Natural Language :: English',
+         'License :: OSI Approved :: MIT License',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+    install_requires=['Flask','MarkupSafe','decorator','itsdangerous','six'],
+)
