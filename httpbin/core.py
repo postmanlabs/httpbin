bd43eb5b86c58f6060904bac9f9f2a6c01fd6e31
Todd Menier <tmenier@gmail.com>
2015-05-09 10:54:18 -0500

    Allow any HTTP method in /delay endpoint
    Issue #224

--- httpbin/core.py
+++ httpbin/core.py
@@ -439,7 +439,7 @@ def digest_auth(qop=None, user='user', passwd='passwd'):
     return jsonify(authenticated=True, user=user)
 
 
-@app.route('/delay/<int:delay>')
+@app.route('/delay/<int:delay>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'TRACE'])
 def delay_response(delay):
     """Returns a delayed response"""
     delay = min(delay, 10)
