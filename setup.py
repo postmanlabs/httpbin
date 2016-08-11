9259ce013695e5238d9fbefb3b369bc283f322fd
Ian Cordasco <graffatcolmingov@gmail.com>
2016-08-10 19:49:47 -0500

    Bump version to 0.5.0
    Hopefully we can get a release out on Monday

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
-import codecs
 import os
-import re
 
 long_description = open(
     os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name="httpbin",
-    version="0.4.1",
+    version="0.5.0",
     description="HTTP Request and Response Service",
     long_description=long_description,
 
