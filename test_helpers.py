19397150d167daf6bdbc4a3b133018ea5194adda
Ed Singleton <singletoned@gmail.com>
2017-12-04 09:00:03 +0000

    Add test for digest_challenge_response

--- test_helpers.py
+++ test_helpers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from httpbin.helpers import parse_multi_value_header
+from werkzeug import Request
+
+from httpbin.helpers import parse_multi_value_header, digest_challenge_response
 
 
 def test_parse_multi_value_header():
@@ -10,3 +12,14 @@ def test_parse_multi_value_header():
     assert parse_multi_value_header('"xyzzy" == "r2d2xxxx", "c3piozzzz"'), ["xyzzy", "r2d2xxxx", "c3piozzzz"]
     assert parse_multi_value_header('W/"xyzzy" == W/"r2d2xxxx", W/"c3piozzzz"'), ["xyzzy", "r2d2xxxx", "c3piozzzz"]
     assert parse_multi_value_header('*') == ["*"]
+
+
+def test_digest_challenge_response():
+    request = Request(environ={'REMOTE_ADDR': "example.com"})
+    result = digest_challenge_response(request, None, None)
+    assert '''qop="auth, auth-int"''' in result.headers["WWW-Authenticate"]
+    assert '''algorithm=''' not in result.headers["WWW-Authenticate"]
+
+    result = digest_challenge_response(request, "flibble", "flamble")
+    assert '''qop="flibble"''' in result.headers["WWW-Authenticate"]
+    assert '''algorithm=flamble''' in result.headers["WWW-Authenticate"]
