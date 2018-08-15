10940166f8302d0419630a2958ef4ecc476285af
Brett Randall <javabrett@gmail.com>
2018-08-15 15:34:06 +1000

    Added basic-auth path to allow testing with empty password. Fixed #502.

--- httpbin/core.py
+++ httpbin/core.py
@@ -941,8 +941,9 @@ def delete_cookies():
     return r
 
 
+@app.route("/basic-auth/<user>")
 @app.route("/basic-auth/<user>/<passwd>")
-def basic_auth(user="user", passwd="passwd"):
+def basic_auth(user="user", passwd=""):
     """Prompts the user for authorization using HTTP Basic Auth.
     ---
     tags:
