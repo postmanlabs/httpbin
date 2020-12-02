d54d31066db37f3c46a29e187386499475da8998
Michał Górny <mgorny@gentoo.org>
2020-12-02 10:42:52 +0100

    Replace brotlipy with brotlicffi
    The brotlipy package has been renamed to brotlicffi.  Update the imports
    and dependencies accordingly.  The major advanage of the new package
    is that it no longer collides with the Python bindings provided
    by brotli itself.

--- httpbin/filters.py
+++ httpbin/filters.py
@@ -10,7 +10,7 @@ This module provides response filter decorators.
 import gzip as gzip2
 import zlib
 
-import brotli as _brotli
+import brotlicffi as _brotli
 
 from six import BytesIO
 from decimal import Decimal
